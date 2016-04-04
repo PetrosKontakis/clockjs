3cdd577a4a92b95d1d25fe0e89c3c3b251cb5434
PetrosKontakis <petroskontakis@gmail.com>
2016-04-04 19:28:50 +0300

    Revert "Revert "first-upload""
    This reverts commit 6581308104ff06f7907661ce2266a412fadd133e.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*
+* Crated by Petros Kontakis on 7-3-2016
+*
+*
+*/
+
+var app = angular.module("pkclockPresentation", ["ngRoute"]);
+
+app.controller("MainControler", ["$scope", function($scope){
+	$scope.test= 'tada';
+
+}]);
